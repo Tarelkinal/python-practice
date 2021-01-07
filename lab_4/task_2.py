273ee29e206285c59d95f076aafc852d2677b680
Tarelkinal <alekandr.tarelkin@gmail.com>
2021-01-07 19:08:29 +0300

    lab_4 task_2

---
+++ lab_4/task_2.py
@@ -0,0 +1,11 @@
+from math import fabs, cos, sin, radians, log2, sqrt, log, exp, pow
+
+
+def func_1(x, a, k, y):
+    res = sqrt(fabs(cos(x**2 + radians(44)) + a * sin(k * y)**2)) - 0.6 * y**3 + log2(8)/(4 * a)
+    return res
+
+
+def func_2(x, y):
+    res = exp(fabs(4*y - 0.5)) + pow(x, 1/3)/(1 + log(2*x))
+    return res
