0005401ad48a6162df2ac503732c89cbc01e24c5
Tarelkinal <alekandr.tarelkin@gmail.com>
2021-01-20 11:48:08 +0300

    lab_4 finished

--- lab_4/task_4.py
+++ lab_4/task_4.py
@@ -1,7 +1,8 @@
 import os
+from pprint import pprint
 
 
-def make_dir():
+def make_dirs():
     struct = {'СЕМЕСТР 1': ['python', 'algorithms', 'math'],
               'СЕМЕСТР 2': ['C++', 'Java', 'physics']}
 
@@ -14,5 +15,26 @@ def make_dir():
                 f_in.write(f'{path}\\path.txt')
 
 
+def get_dirs_dict(path=os.getcwd()):
+    dir_li = [x for x in os.scandir(path) if x.is_dir()]
+    files_li = [x for x in os.scandir(path) if x.is_file()]
+    if not dir_li:
+        if not files_li:
+            return None
+        else:
+            result = files_li[0].name if len(files_li) == 1 else list(map(lambda x: x.name, files_li))
+            return result
+
+    if len(dir_li) == 1 and len(files_li) == 0:
+        return {dir_li[0].name: get_dirs_dict(path + '\\' + '\\' + dir_li[0].name)}
+    else:
+        result = []
+        for d in dir_li:
+            result.append({d.name: get_dirs_dict(path + '\\' + d.name)})
+        for f in files_li:
+            result.append(f.name)
+        return result
+
+
 if __name__ == '__main__':
-    make_dir()
+    pprint(get_dirs_dict())
